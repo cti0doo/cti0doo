7ebad918d7e5fec590548386bd27f017236a81a9
Rafael Riveros <cti@cti.com.co>
2024-06-20 19:33:58 -0500

    Actualizando a la última versión de cti0doo

---
+++ l10n_co_product_category_taxes/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Product Category Taxes',
+    'version': '0.0.1',
+    'author': 'CTI Ltda - based on IT-Projects LLC, Ivan Yelizariev',
+    'summary': 'Adding taxes into product category',
+    'license': 'LGPL-3',
+    'category': 'Accounting',
+    'website': 'https://cti.com.co',
+    'depends': ['base','account'],
+    'data': [
+        'views/views.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ l10n_co_products_collect_supply/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Product Collect & Supply",
+
+    'summary': """
+        Module that allows to register collect of the same product to several
+        suppliers in a simple form and supplies management
+    """,
+
+    'description': """
+        Module that allows to register collect of the same product to several
+        suppliers in a simple form and supplies management
+    """,
+
+    'author': "CTI Ltda",
+    'website': "http://www.cti.com.co",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Operations/Purchase/inventory/Deliver',
+    'version': '17.0',
+    'license': 'OPL-1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','account', 'purchase', 'stock_landed_costs', 'stock_picking_batch','sale_stock','sale_renting'],
+
+    # always loaded
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'reports/purchase_line_pivot.xml',
+        'data/sequence.xml',
+        'reports/product_collect_templates.xml',
+        'reports/product_collect_reports.xml',
+        'views/data.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
